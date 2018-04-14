9136445bcae97a468856db42da1974530ecf62d0
shanistud <shanis@Shanis-MacBook-Pro-2.local>
2018-04-14 15:10:50 +0300

    first commit

---
+++ bin/github-rank.js
@@ -0,0 +1,36 @@
+const { rankGithubReposByUser } = require('../lib')
+
+
+
+if (process.argv.length < 3) {
+    quitWithError('please defin a github url');
+}
+
+githubUrl = process.argv[2];
+
+const githubUser = grabGithubUserFromUrl(githubUrl);
+if (!githubUser) {
+    quitWithError('invalid github repo index url');
+}
+
+rankGithubReposByUser(githubUser, (err, res) => {
+    if (err) {
+        quitWithError(err.message);
+    }
+
+    console.log(res);
+})
+
+
+
+
+function quitWithError (msg) {
+    console.log(msg);
+    process.exit(1);
+}
+
+function grabGithubUserFromUrl (githubUrl) {
+    const validationExpression = /^(https|http)\:\/\/(www\.)?github\.com\/(\w+$)/i;
+    let match = githubUrl.match(validationExpression)
+    return  match != null ? match[3] : null;
+}
